5766857673ee352f5f7d7a6cd4cca90736a8d889
Evan K <ekerik220@gmail.com>
2020-04-16 13:35:16 +0900

    Updates

---
+++ precache-manifest.9ed0acfddc46782b00b123c8bcc762d2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4c8d540b3e8b87000922e86975355d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8eb89f9daf3824d54f35",
+    "url": "/portfolio/static/css/main.229c8449.chunk.css"
+  },
+  {
+    "revision": "d500096abc99c659bd55",
+    "url": "/portfolio/static/js/2.f7721e74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.f7721e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb89f9daf3824d54f35",
+    "url": "/portfolio/static/js/main.93ed4d25.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  },
+  {
+    "revision": "837b4f34ab6b2173d3bd9a41b3c8e742",
+    "url": "/portfolio/static/media/calculator-preview.837b4f34.png"
+  },
+  {
+    "revision": "a3487d3f2480263249dc02ddb6bbdf4c",
+    "url": "/portfolio/static/media/first-trend-preview.a3487d3f.png"
+  },
+  {
+    "revision": "c39f33b0e8248bdae7b9ceada8505279",
+    "url": "/portfolio/static/media/portrait.c39f33b0.jpg"
+  },
+  {
+    "revision": "06dcbe5f54122ddb68290e843e971e39",
+    "url": "/portfolio/static/media/recipe-finder-preview.06dcbe5f.png"
+  },
+  {
+    "revision": "24b249d40b4ad8450c8e0caf3a34113b",
+    "url": "/portfolio/static/media/weather-preview.24b249d4.png"
+  }
+]);
