32e0dfc835d739ccef42d30ad22fb85cfadae171
Evan Kerik <ekerik220@gmail.com>
2020-06-14 18:23:40 +0900

    Updates

---
+++ precache-manifest.a725c84b53d6c435b9693b83a346232a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e015c424924800052714ceb8ab9d29",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b283b38a86751802ebec",
+    "url": "/portfolio/static/css/main.944ca523.chunk.css"
+  },
+  {
+    "revision": "d500096abc99c659bd55",
+    "url": "/portfolio/static/js/2.f7721e74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.f7721e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b283b38a86751802ebec",
+    "url": "/portfolio/static/js/main.c660b068.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  },
+  {
+    "revision": "8591a83fefdfac1a6cf93ebe29fe7703",
+    "url": "/portfolio/static/media/acnh-tracker.8591a83f.mp4"
+  },
+  {
+    "revision": "93eaf5c7b4e246b72f7125fa035ca5d2",
+    "url": "/portfolio/static/media/calculator-preview.93eaf5c7.png"
+  },
+  {
+    "revision": "a95ee2aba4f4071bda6c2485c4ae1d63",
+    "url": "/portfolio/static/media/first-trend-preview.a95ee2ab.png"
+  },
+  {
+    "revision": "0d2b582c433bf8d141b748457fb3fc92",
+    "url": "/portfolio/static/media/portrait.0d2b582c.jpg"
+  },
+  {
+    "revision": "f9f610482afd84683e8bdc6f3e567441",
+    "url": "/portfolio/static/media/recipe-finder-preview.f9f61048.png"
+  },
+  {
+    "revision": "d16f548d77cd7fcf1a067226b10eb204",
+    "url": "/portfolio/static/media/weather-preview.d16f548d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9b5176ca2148376fc68998c93cbada83.js"
+  "/portfolio/precache-manifest.a725c84b53d6c435b9693b83a346232a.js"
 );
 
 self.addEventListener('message', (event) => {
